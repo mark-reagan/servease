f665ee98775f825a99aed5b21b4eb5c7a84ba105
Mark Reagan <171141812+zxcvbnriggx@users.noreply.github.com>
2026-09-25 03:16:51 +0800

    cleanup cors

--- server/config/cors.php
+++ server/config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_origins' => array_values(array_filter(array_map(
         'trim',
-        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173')),
+        explode(',', env('CORS_ALLOWED_ORIGINS')),
     ))),
 
     'allowed_origins_patterns' => [],
